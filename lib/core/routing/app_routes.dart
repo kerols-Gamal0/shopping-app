49b7c3665c3cf8ad4d63d4a8b54da927ed80a0aa
bankai <hassanamr841981@gmail.com>
2026-07-24 22:47:51 +0300

    fix: define application route constants in AppRoutes class

--- lib/core/routing/app_routes.dart
+++ lib/core/routing/app_routes.dart
@@ -2,5 +2,4 @@ abstract class AppRoutes {
   static const String homeRoute = "/home";
   static const String onboardingRoute = "/onboarding";
   static const String testRoute = "/test";
-  static const String homeRouteV2 = "/home_v2";
 }
