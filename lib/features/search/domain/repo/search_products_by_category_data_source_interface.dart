8ce13e6150722212e1e5c29e94d00da7273c0792
AyaAbdElmoneim158 <ayaabdelmon@gmail.com>
2026-07-29 14:59:14 +0300

    feat: add domain layer

---
+++ lib/features/search/domain/repo/search_products_by_category_data_source_interface.dart
@@ -0,0 +1,6 @@
+import 'package:shopping_app/core/common/model/product_item/product_item_dto.dart';
+import 'package:shopping_app/features/search/data/models/search_products_by_category_request_body.dart';
+
+abstract interface class SearchProductsByCategoryDataSourceInterface {
+   Future<List<ProductItemDto>> searchProductsByCategory(SearchProductsByCategoryRequestBody body);
+}
